b8e4aad560559251e630ccaca2e7b28fd4fef587
remotevase <web.articlerequests@gmail.com>
2016-05-11 17:37:27 -0400

    Looks better

--- src/HomeTP/HomeTP.php
+++ src/HomeTP/HomeTP.php
@@ -71,7 +71,7 @@ class HomeTP extends PluginBase{
             }
         }
     public function onDisable(){
-        $this->getLogger()->info(C::RED. "HomeTeleporter has successfully Disabled!");
+        $this->getLogger()->info(C::DARK_RED. "HomeTeleporter has successfully Disabled!");
         $this->saveResource("homes.yml");
         $this->getLogger()->info(C::YELLOW."All homes have saved!");
     }

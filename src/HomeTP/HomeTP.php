6f47cfd490c2a2dc7d9e830128c96aff44d692a2
remotevase <web.articlerequests@gmail.com>
2016-05-11 17:29:24 -0400

    Update HomeTP.php

--- src/HomeTP/HomeTP.php
+++ src/HomeTP/HomeTP.php
@@ -66,7 +66,7 @@ class HomeTP extends PluginBase{
                     if($home["world"] instanceof Level){
                         $sender->sendMessage(C::BLUE."Yes, " . $args[0] . "is a house. It's location is " . $home["x"] ." " . $home["y"] . " " . $home["z"]. "In the world " . $home["world"]);
                 }else{
-                    $sender->sendMessage(C::BLUE. "No, " . $args[0] . "is not a house. Use /sethome " . $args[0] . "to set a home with the name of " . $args[0]);
+                    $sender->sendMessage(C::BLUE. "No,-" . $args[0] . "is not a house. Use '/sethome'-'" . $args[0] . "' -to set a home with the name of-" . $args[0]);
                 }
             }
         }
